db3b1537d2a150101dbe91cc23ebcb01a9b8380b
Jonathan Slack <slack6000@gmail.com>
2022-05-19 09:33:49 -0500

    second commit

---
+++ printDigits.js
@@ -0,0 +1,11 @@
+let num = 314;
+
+function printDigits(num) {
+    let rev_num = 0; 
+    while (num > 0) {
+        rev_num = rev_num * 10 + num % 10; 
+        num = Math.floor(num / 10)
+    }
+    return rev_num
+}
+console.log(printDigits(num))
